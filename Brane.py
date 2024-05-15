Debuging wavelet gradients. gradient surface seems to have wrong scale or coordinate source.

Brane.py
@@ -62,9 +62,9 @@ class Brane(pygame.sprite.Sprite):
         self.elapsed += dt
         if(self.elapsed > 2000):
             self.elapsed = 0.
-#            R = np.random.random((2))*SIM_SIZE
-#            wl = Wavelet(v = VV, L = LL, A=2.5, source=R)
-#            wl.register(self)
+            R = np.random.random((2))*SIM_SIZE
+            wl = Wavelet(v = VV, L = LL, A=2.5, source=R)
+            wl.register(self)
 
 
     def draw(self, view):
@@ -100,7 +100,7 @@ class Brane(pygame.sprite.Sprite):
         # overwtite grad_surf's alpha
         gsa = np.array(grad_surf.get_view('A'), copy=False)
         gsa[:,:] = grad_alpha
-        del gsa # to unlcok surface
+        del gsa # to unlock grad_surf to allow blit 
         
         # draw onto intensity's surface
         amp_surf.blit(grad_surf, grad_surf.get_rect())

wavelets/Tractor.py
@@ -135,7 +135,7 @@ class Tractor(Wavelet, pygame.sprite.Sprite):
 #            print("rprime", rprime.shape)
 #            print("rprimeLen", rprimeLen.shape)
 #            print("rprimeLen[mask]", rprimeLen[mask].shape)
-#            print(mask)
+
             
             # distance dependence from source
             Id = self.A*(np.sqrt(self.Rmax) - np.sqrt(rprimeLen[mask]))
@@ -149,18 +149,18 @@ class Tractor(Wavelet, pygame.sprite.Sprite):
             
             
             # gradients of components
-            gradrprime = - rprime[mask]/np.sqrt(rprimeLen[mask])
+            gradrprime = - rprime[mask]/np.sqrt(rprimeLen[mask])[:,np.newaxis]
             
             gradId = self.A*(np.sqrt(self.Rmax) + 0.5/np.sqrt(rprimeLen[mask]))
             gradId = gradId[:,np.newaxis] * gradrprime
 #            print("gradId", gradId.shape)
-            gradIa = (self.dir - cosTheta*gradrprime)/rprimeLen[mask]
+            gradIa = (self.dir[np.newaxis,:] - cosTheta[:,np.newaxis]*gradrprime)/rprimeLen[mask][:,np.newaxis]
 #            print("gradIa", gradIa.shape)
             
             relpos = rprimeLen[mask] - self.v*self.lifetime
             gradW = np.where(relpos>0, -2.0/self.L, 0.0) # 0 at peak of np.abs
             gradW = np.where(relpos<0, +2.0/self.L, gradW)
-            gradW = gradW * gradrprime
+            gradW = gradW[:,np.newaxis] * gradrprime
 #            print("gradW", gradW.shape)
             
     
@@ -168,6 +168,8 @@ class Tractor(Wavelet, pygame.sprite.Sprite):
             Ia = Ia[:,np.newaxis]
             Id = Id[:,np.newaxis]
             W = W[:,np.newaxis]
+#            print("Ia", Ia.shape)
+#            print("Id", Id.shape)
 #            print("W", W.shape)
 #            print("G", G.shape)
 #            print("mask", mask.shape)