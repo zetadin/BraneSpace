Fixed Tractor beam gradient

Brane.py
@@ -75,7 +75,8 @@ class Brane(pygame.sprite.Sprite):
         screen = view.displaysurface
         
         # re-paint the surface from simulation
-        amp_arr = np.floor(np.clip(256*(self.I+1)/2, 0,255)).astype(np.uint8)
+#        self.I = np.zeros(self.I.shape)
+        amp_arr = np.floor(np.clip(256*(self.I*4.0+1)/2, 0,255)).astype(np.uint8)
         amp_arr = np.repeat(amp_arr[:, :, np.newaxis], 3, axis=2)
         amp_surf = pygame.surfarray.make_surface(amp_arr)
         
@@ -83,14 +84,13 @@ class Brane(pygame.sprite.Sprite):
         gcoords = self.coords.reshape((-1,2))
         grad_arr = self.computeForceAt(gcoords*self.surfScale)
         grad_arr = grad_arr.reshape((SIM_SIZE,SIM_SIZE,2))
-#        grad_I = np.linalg.norm(grad_arr, axis=-1)
         grad_colors = np.zeros((SIM_SIZE,SIM_SIZE,3)).astype(np.uint8)
         # x -> red
         grad_colors[:,:,0] = np.floor(np.clip(256*(grad_arr[:,:,0]+1)/2, 0,255)).astype(np.uint8)
         # y -> green
         grad_colors[:,:,1] = np.floor(np.clip(256*(grad_arr[:,:,1]+1)/2, 0,255)).astype(np.uint8)
         # alpha
-        grad_alpha = np.max(grad_colors[:,:,0:1], axis=-1)
+        grad_alpha = np.floor(np.clip(np.max(np.abs(grad_arr), axis=-1), 0.0, 0.1)*2550).astype(np.uint8)
     
         # new surface for gradient
         grad_surf = pygame.Surface((SIM_SIZE,SIM_SIZE), pygame.SRCALPHA, 32)

entities/Player.py
@@ -21,10 +21,11 @@ class Player(SpriteEntity):
         self.size = 64 # px
         
         # physics properties
-        self.mass = 5.0e3
+        self.mass = 5.0e8
         self.dragCoef = 0.2
         # coordinates in world space
         self.r = np.array([WIDTH/3, WIDTH/3])
+        self.theta = -np.pi*0.5
         
         self.tractorElapsed = 0.0
         
@@ -33,14 +34,15 @@ class Player(SpriteEntity):
         super().update(dt)
         
         # spin ship for demo of rotation
-        self.theta -= dt*np.pi/2000. # 180 deg in 2 sec
+        self.theta -= dt*np.pi/4000. # 180 deg in 2 sec
         
         
         # every L/(2*v) seconds emit a tractor wavelet
         
         self.tractorElapsed += dt
-        if(self.tractorElapsed > 1000):
-            self.tractorElapsed = 0.
+        pulseTime = 16.0*0.5/3.2e-2
+        if(self.tractorElapsed > pulseTime):
+            self.tractorElapsed -= pulseTime
             
             # where the beam is heading
             direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
@@ -50,8 +52,11 @@ class Player(SpriteEntity):
             wl = Tractor(source=start, direction=direction,
                          v = 3.2e-2,
                          L = 16,
-                         A = 0.3,
+                         A = 0.1,
                          Rmax = 128./self.parentBrane.surfScale, # in sim coords
                          debug=True)
+            wl.alive = self.tractorElapsed
             wl.register(self.parentBrane)
         
+        
+            

wavelets/Tractor.py
@@ -149,17 +149,25 @@ class Tractor(Wavelet, pygame.sprite.Sprite):
             
             
             # gradients of components
-            gradrprime = - rprime[mask]/np.sqrt(rprimeLen[mask])[:,np.newaxis]
+            gradrprime = rprime[mask]/rprimeLen[mask][:,np.newaxis]
             
             gradId = self.A*(np.sqrt(self.Rmax) + 0.5/np.sqrt(rprimeLen[mask]))
             gradId = gradId[:,np.newaxis] * gradrprime
+            gradId = np.where(rprimeLen[mask][:,np.newaxis] < self.Rmax, gradId, 0.0)
+            
 #            print("gradId", gradId.shape)
-            gradIa = (self.dir[np.newaxis,:] - cosTheta[:,np.newaxis]*gradrprime)/rprimeLen[mask][:,np.newaxis]
+#            gradIa = (self.dir[np.newaxis,:] - cosTheta[:,np.newaxis]*gradrprime)/rprimeLen[mask][:,np.newaxis]
+            gradIa = self.dir[np.newaxis,:] - (cosTheta[:,np.newaxis]*gradrprime)/(rprimeLen[mask]*rprimeLen[mask])[:,np.newaxis]
+            # apply the max for angular dependence too
+            gradIa = np.where(cosTheta[:,np.newaxis] > np.cos(self.theta0),
+                             gradIa, 0.0)
 #            print("gradIa", gradIa.shape)
             
             relpos = rprimeLen[mask] - self.v*self.lifetime
-            gradW = np.where(relpos>0, -2.0/self.L, 0.0) # 0 at peak of np.abs
-            gradW = np.where(relpos<0, +2.0/self.L, gradW)
+            gradW = np.where(np.logical_and(relpos>0, relpos<0.5*self.L),
+                             -2.0/self.L, 0.0) # 0 at peak of np.abs
+            gradW = np.where(np.logical_and(relpos<0, relpos>-0.5*self.L),
+                             +2.0/self.L, gradW)
             gradW = gradW[:,np.newaxis] * gradrprime
 #            print("gradW", gradW.shape)
             
@@ -175,6 +183,8 @@ class Tractor(Wavelet, pygame.sprite.Sprite):
 #            print("mask", mask.shape)
 #            print("G[mask]", G[mask].shape)
             G[mask] = (gradId*Ia + Id*gradIa)*W + Id*Ia*gradW
+            
+#            G[mask] = gradIa
         
         return(G)
     