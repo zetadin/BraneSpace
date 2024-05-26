fix sprite CCW rotation and movement direction.

entities/Entity.py
@@ -8,7 +8,6 @@ Created on Fri May 10 13:31:16 2024
 
 import numpy as np
 import pygame
-from pygame.locals import *
 
 from Brane import Brane
 from Universe import updatables, drawables
@@ -68,7 +67,7 @@ class SpriteEntity(Entity, pygame.sprite.Sprite):
         super().__init__(mass, drag)
         
         # create a temp image that will be overriden
-        self.img = pygame.Surface((16,16), flags=SRCALPHA)
+        self.img = pygame.Surface((16,16), flags=pygame.SRCALPHA)
         pygame.draw.rect(
                 surface=self.img, color=(255, 0, 0, 255),
                 rect=self.img.get_rect(),
@@ -87,9 +86,10 @@ class SpriteEntity(Entity, pygame.sprite.Sprite):
         if(view.isOnScreen(self)):
             # scale & rotate the image
             zoom = float(self.size)*view.zoom/self.img.get_width()
-            surf = pygame.transform.rotozoom(self.img,
-                                             self.theta*180/np.pi, # in deg
-                                             zoom)
+            surf = pygame.transform.rotozoom(
+                    self.img,
+                    -self.theta*180./np.pi, # in deg CCW of North (Up)
+                    zoom)
             
             # update position on screen
             rect = surf.get_rect(center=view.transform(self.r))

entities/Player.py
@@ -25,7 +25,7 @@ class Player(SpriteEntity):
         self.dragCoef = 0.02
         # coordinates in world space
         self.r = np.array([WIDTH/3, WIDTH/3])
-        self.theta = -np.pi*0.5
+        self.theta = 0 #np.pi*0.5
         
         self.tractorElapsed = 0.0
         self.tractorActive = True
@@ -44,8 +44,8 @@ class Player(SpriteEntity):
         self.bck = False
         
         # collector motion
-        direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
-        self.collector_r = self.r + self.collect_radius*direction
+        self.direction = np.array([np.sin(self.theta), -np.cos(self.theta)])
+        self.collector_r = self.r + self.collect_radius*self.direction
         self.collector_v = np.zeros(2)
         
     def calcForce(self):
@@ -53,20 +53,23 @@ class Player(SpriteEntity):
         # remove the extra dimention used for multiple points
         F = np.squeeze(F, axis=0)
         
+
         if(self.fwd):
-            direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
-            F += direction * self.fwdThrust
+            F += self.direction * self.fwdThrust
         if(self.bck):
-            direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
-            F -= direction * self.bckThrust
+            F -= self.direction * self.bckThrust
         
         return(F)
         
     def update(self, dt: float):
+        # facing unit vector
+        self.direction = np.array([np.sin(self.theta), -np.cos(self.theta)])
         super().update(dt)
         
         # update ship rotation        
         self.theta += dt*self.rot_speed*self.rotationDirection
+        
+        
         
         # every L/(2*v) seconds emit a tractor wavelet
         self.tractorElapsed += dt
@@ -74,12 +77,10 @@ class Player(SpriteEntity):
         if(self.tractorElapsed > pulseTime):
             self.tractorElapsed -= pulseTime
             
-            # where the beam is heading
-            direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
             # source of the wave in sim coords
             # a bit forward of player ship
-            start = self.r/self.parentBrane.surfScale + 3*direction 
-            wl = Tractor(source=start, direction=direction,
+            start = (self.r + 20*self.direction)/self.parentBrane.surfScale
+            wl = Tractor(source=start, direction=self.direction,
                          v = 3.2e-2,
                          L = 8.0,
                          A = 0.1,
@@ -90,8 +91,7 @@ class Player(SpriteEntity):
         
         
         # collector motion
-        direction = np.array([-np.sin(self.theta), -np.cos(self.theta)])
-        collector_newr = self.r + self.collect_radius*direction
+        collector_newr = self.r + self.collect_radius*self.direction
         self.collector_v = (collector_newr - self.collector_r)/dt
         self.collector_r = collector_newr
         
@@ -100,31 +100,30 @@ class Player(SpriteEntity):
         Draw to screen
         """
         super().draw(view)
+        print("theta:", self.theta*180./np.pi, "\tdirection:", self.direction)
         
         # Debug shapes
         # collector zone
-#        collector_zone = pygame.Surface(
-#                    (2*self.collect_radius, 2*self.collect_radius),
-#                    flags=pygame.SRCALPHA)
         pygame.draw.circle(view.displaysurface, (0,0,255),
-                           (self.collector_r[0]+self.collect_radius,
-                            self.collector_r[1]+self.collect_radius),
+                           (self.collector_r[0],
+                            self.collector_r[1]),
                            self.collect_radius
                           )
-#        rect = collector_zone.get_rect(center=view.transform(self.collector_r))
-#        view.displaysurface.blit(collector_zone, rect)
-        
+                           
         # collector velocity
-        L = np.linalg.norm(self.collector_v)*10
+        L = np.ceil(np.linalg.norm(self.collector_v)*500)
         collector_vel = pygame.Surface((6, L), flags=pygame.SRCALPHA)
         pygame.draw.polygon(collector_vel, (0,255,0),
                            #((2,L),(2,2), (0,2),(3,0),(5,2), (3,2),(3,L)),
                            ((0,L),(3,0),(5,L)),
-                           width = 1
+                           width = 2
                           )
+        angle = np.arctan(self.collector_v[0]/self.collector_v[1])
+        if(self.collector_v[1]<0): # compensate for period of tan
+            angle+=np.pi
         collector_vel = pygame.transform.rotozoom(
                 collector_vel,
-                -np.arctan(self.collector_v[0]/self.collector_v[1])*180/np.pi, # in deg CCW
+                angle*180/np.pi, # in deg CCW
                 1
                 )
         rect = collector_vel.get_rect(center=view.transform(self.collector_r))

entities/structures/Portal.py
@@ -28,4 +28,4 @@ class Portal(Structure):
         super().update(dt)
         
         #rotation
-        self.theta += dt*np.pi/1000. # 180 deg/s
+        self.theta -= dt*np.pi/1000. # 180 deg/s